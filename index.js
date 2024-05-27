982c6debd15d0a198846296fa8a1f37ab4cb791a
Kishore Kuppan <kkuppan@vmware.com>
2024-05-27 17:34:51 +0530

    feature: basic app

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from kishore sample app')
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
