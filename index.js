2776e2a4e8c48289d280fca76c8e7931f97e22d7
Kishore Kuppan <kkuppan@vmware.com>
2024-06-01 02:37:15 +0530

    Index js revert

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 require('dotenv').config();
 require('./db');
-
 const PORT = process.env.PORT || 8080;
-
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
-const admin_approval = require('./conrollers/Admin_Approval')
-
-app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -20,13 +14,10 @@ app.get('/', (req, res) => {
 app.get('/ping', (req, res) => {
     res.send('PONG')
 });
-
 // /products
 app.use('/products', productRoutes);
 // /users
 app.use('/users', userRoutes);
-// admin approval
-app.use('/admin',admin_approval);
 
 app.listen(8080, () => {
     console.log('Server is listenin on PORT :' + PORT);
